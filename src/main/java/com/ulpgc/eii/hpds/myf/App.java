51d76c7ec080dd29490bd9e3da129ef01329bdd3
Mikota <myf1995@gmail.com>
2017-11-08 20:25:03 +0000

    Initial Commit

---
+++ src/main/java/com/ulpgc/eii/hpds/myf/App.java
@@ -0,0 +1,13 @@
+package com.ulpgc.eii.hpds.myf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
